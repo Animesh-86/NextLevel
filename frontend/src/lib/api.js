2026d0792bd932ba6b7de5af90f46cbf032ebe63
Animesh <animesh8sharma@gmail.com>
2026-05-29 17:48:10 +0530

    updates

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 const DEFAULT_API_BASE_URL = 'http://localhost:8080';
 
 export function getApiBaseUrl() {
-  return (process.env.NEXT_PUBLIC_API_BASE_URL || DEFAULT_API_BASE_URL).replace(/\/$/, '');
+  return (process.env.NEXT_PUBLIC_API_BASE_URL || process.env.NEXT_PUBLIC_API_URL || DEFAULT_API_BASE_URL).replace(/\/$/, '');
 }
 
 export function getTokenFromCookie() {
